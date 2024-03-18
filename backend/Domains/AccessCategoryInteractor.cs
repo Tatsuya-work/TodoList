6f74de9a7fe02e9c709aa5d8801ec4955cd076f4
Ishizaki Tatsuya <tatsu.maro@icloud.com>
2024-03-18 17:23:39 +0900

    側作成

---
+++ backend/Domains/AccessCategoryInteractor.cs
@@ -0,0 +1,18 @@
+﻿using TodoList.Usecases;
+using TodoList.Data.Class;
+
+namespace TodoList.Domains
+{
+    public class AccessCategoryInteractor : IAccessCategoryUsecase
+    {
+        public void AddCategory(string category)
+        {
+
+        }
+
+        public string[] GetCategory()
+        {
+
+        }
+    }
+}
